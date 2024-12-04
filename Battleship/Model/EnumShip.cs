f667c8499292ef23379552a5de3edaf76cda5f18
Triadziuch <kurowski1234567890@gmail.com>
2024-12-04 09:37:49 +0100

    Singleton AssetManger basic implementation. Enum ship types.

---
+++ Battleship/Model/EnumShip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public enum ShipType
+    {
+        Carrier = 5,
+        Battleship = 4,
+        Cruiser = 3,
+        Submarine = 3,
+        Destroyer = 2
+    }
+}
---
+++ Battleship/Pattern/AssetManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Battleship.Pattern
+{
+    public class AssetManager
+    {
+        private Dictionary<string, string> m_Skins;
+        private static readonly AssetManager sInstance = new AssetManager();
+
+        public AssetManager()
+        {
+            m_Skins = new Dictionary<string, string>();
+        }
+
+        public static AssetManager Instance
+        {
+            get { return sInstance; }
+        }
+
+        public string GetSkin(string filename)
+        {
+            if (m_Skins.TryGetValue(filename, out var skin))
+                return skin;
+            else
+                throw new KeyNotFoundException($"Skin '{filename}' not found.");
+        }
+
+        public void AddSkin(string filename, string skin)
+        {
+            if (!m_Skins.ContainsKey(filename))
+                m_Skins[filename] = skin;
+        }
+    }
+}
